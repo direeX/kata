1d1477e21ece56253494863c3b0507ba6b7cb180
direeX <dominik.obara7@gmail.com>
2023-02-03 08:48:53 +0100

    kata

--- src/kata/Kata.java
+++ src/kata/Kata.java
@@ -1,2 +1,290 @@
-package kata;public class Kata {
+package kata;
+// #zad 1 (8)
+//We need a simple function that determines if a plural is needed or not. It should take a number, and return true if a plural should be used with that number or false if not. This would be useful when printing out a string such as 5 minutes, 14 apples, or 1 sun.
+// You only need to worry about english grammar rules for this kata, where anything that isn't singular (one of something), it is plural (not one of something).
+//  All values will be positive integers or floats, or zero.
+/*
+public class Kata {
+    public static void main(String[] args) {
+    }
+    public static boolean isPlural(float f){
+        return f != 1;
+    }
 }
+*/
+
+//**
+// #zad 2 (8)
+/*
+You will be given a list of strings. You must sort it alphabetically (case-sensitive, and based on the ASCII values of the chars) and then return the first value.
+ The returned value must be a string, and have "***" between each of its letters.
+ You should not remove or add elements from/to the array.
+import java.util.Arrays;
+public class Kata {
+    public static void main(String[] args) {
+        String[] arr = { "are",
+                "www.geeksforgeeks.org",
+                "code.geeksforgeeks.org" };
+        twoSort(arr);
+    }
+    public static String twoSort(String[] s) {
+        java.util.Arrays.sort(s);
+        return String.join("***", s[0].split(""));
+    }
+}
+ */
+
+// #zad 3 (7)
+/*
+My grandfather always predicted how old people would get, and right before he passed away he revealed his secret!
+In honor of my grandfather's memory we will write a function using his formula!
+Take a list of ages when each of your great-grandparent died.
+Multiply each number by itself.
+Add them all together.
+Take the square root of the result.
+Divide by two.
+Example
+predictAge(65, 60, 75, 55, 60, 63, 64, 45) === 86
+Note: the result should be rounded down to the nearest integer.
+Some random tests might fail due to a bug in the JavaScript implementation. Simply resubmit if that happens to you.
+
+
+public class Kata {
+    public static int predictAge(int age1, int age2, int age3, int age4, int age5, int age6, int age7, int age8) {
+      int[] arr = {age1, age2, age3, age4, age5, age6, age7, age8};
+      int val = 0;
+      for(int a: arr){
+          val += a * a;
+      }
+      int root = (int) Math.sqrt(val);
+        return root / 2;
+    }
+    public static int squareNumber(int number){
+        return number * number;
+    }
+}
+ */
+
+
+
+/* #zad 4 (8)
+Your task is to sum the differences between consecutive pairs in the array in descending order.
+
+Example
+[2, 1, 10]  -->  9
+In descending order: [10, 2, 1]
+
+Sum: (10 - 2) + (2 - 1) = 8 + 1 = 9
+
+If the array is empty or the array has only one element the result should be 0 (Nothing in Haskell, None in Rust).
+
+
+import java.util.Arrays;
+
+public class Kata {
+    public static void main(String[] args) {
+        sumOfDifferences(new int[] {1, 2, 10});
+    }
+    public static int sumOfDifferences(int[] arr) {
+    Arrays.sort(arr);
+    return arr.length <= 1 ? 0 : arr[arr.length-1] - arr[0];
+
+
+    }
+}
+
+ */
+/* #zad 5 (8)
+Wolves have been reintroduced to Great Britain. You are a sheep farmer, and are now plagued by wolves which pretend to be sheep. Fortunately, you are good at spotting them.
+
+Warn the sheep in front of the wolf that it is about to be eaten. Remember that you are standing at the front of the queue which is at the end of the array:
+
+[sheep, sheep, sheep, sheep, sheep, wolf, sheep, sheep]      (YOU ARE HERE AT THE FRONT OF THE QUEUE)
+   7      6      5      4      3            2      1
+If the wolf is the closest animal to you, return "Pls go away and stop eating my sheep". Otherwise, return "Oi! Sheep number N! You are about to be eaten by a wolf!" where N is the sheep's position in the queue.
+
+Note: there will always be exactly one wolf in the array.
+
+Examples
+Input: ["sheep", "sheep", "sheep", "wolf", "sheep"]
+Output: "Oi! Sheep number 1! You are about to be eaten by a wolf!"
+
+Input: ["sheep", "sheep", "wolf"]
+Output: "Pls go away and stop eating my sheep"
+
+public class Kata {
+    public static String warnTheSheep(String[] array) {
+        //Your code)))
+        for(int i=0; i<array.length-1;i++) {
+            if (array[i].equals("wolf"))
+                return "Oi! Sheep number " + (array.length - i - 1) + "! You are about to be eaten by a wolf!";
+        }
+            return "Pls go away and stop eating my sheep";
+    }}
+ */
+
+/* #zad 6 (8)
+Write a simple regex to validate a username. Allowed characters are:
+
+lowercase letters,
+numbers,
+underscore
+Length should be between 4 and 16 characters (both included).
+public static boolean validateUsr(String s) {
+    return s.matches("[a-z_\\d]{4,16}");
+  }
+ */
+
+/* #zad 7 (8)
+Your car is old, it breaks easily. The shock absorbers are gone and you think it can handle about 15 more bumps before it dies totally.
+
+Unfortunately for you, your drive is very bumpy! Given a string showing either flat road (_) or bumps (n). If you are able to reach home safely by encountering 15 bumps or less, return Woohoo!, otherwise return Car Dead
+
+
+public class Kata {
+    public static String bumps(final String road) {
+
+       int count=0;
+       for(int i =0; i<road.length(); i++){
+           if(road.charAt(i) == 'n')
+               count++;
+       }
+       if(count <= 15){
+           return "Woohoo!";
+       }else
+           return "Car Dead";
+    }
+}
+ */
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/* #zad 9 (7)
+Happy Holidays fellow Code Warriors!
+Santa's senior gift organizer Elf developed a way to represent up to 26 gifts by assigning a unique alphabetical character to each gift. After each gift was assigned a character, the gift organizer Elf then joined the characters to form the gift ordering code.
+
+Santa asked his organizer to order the characters in alphabetical order, but the Elf fell asleep from consuming too much hot chocolate and candy canes! Can you help him out?
+
+Sort the Gift Code
+Write a function called sortGiftCode/sort_gift_code/SortGiftCode that accepts a string containing up to 26 unique alphabetical characters, and returns a string containing the same characters in alphabetical order.
+
+Examples (Input -- => Output):
+"abcdef"                      -- => "abcdef"
+"pqksuvy"                     -- => "kpqsuvy"
+"zyxwvutsrqponmlkjihgfedcba"  -- => "abcdefghijklmnopqrstuvwxyz"
+
+public class Kata{
+    public static void main(String[] args) {
+        sortGiftCode("afdscva");
+    }
+    public static String sortGiftCode(String code){
+        char arr[] = code.toCharArray();
+        Arrays.sort(arr);
+        return new String (arr);
+    }
+}
+
+
+public class Kata{
+    public static void main(String[] args) {
+        greet("Riley");
+    }
+    public static String greet(String name){
+        name = name.substring(0,1).toUpperCase() + name.substring(1).toLowerCase();
+        return "Hello " + name + "!";
+    }
+}
+ */
+
+/*
+#zad 10 (Akademia kodu 1)
+ Dodaj liczby w danym ciągu arytmetycznym:
+ public static int sum(int number){
+
+  int n = 10;
+        int sum = 0;
+    for (int i = 1; i <= n; i++) {
+        sum += i;
+    }
+
+    sum = (n + 1) + n /2;
+    System.out.println(sum);
+        }
+          }
+          return sum;
+ */
+
+
+/*
+#zad 11 (Akademia kodu 2)
+Zamień dwie liczby
+
+ */
+
+class Kata {
+
+
+public static void main(String[]args){
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Podaj pierwszą liczbę");
+    int firstNumber = scanner.nextInt();
+    System.out.println("Podaj drugą liczbę");
+    int secondNumber = scanner.nextInt();
+
+    System.out.println("Wartości przed:");
+    System.out.println(firstNumber + " " + secondNumber);
+
+    int thirdNumber = secondNumber;
+    secondNumber = firstNumber;
+    firstNumber = thirdNumber;
+
+
+    System.out.println("Wartości po zamianie:");
+    System.out.println(firstNumber + " " + secondNumber);
+}
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
